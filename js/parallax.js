2a44149df752ebd5eb0579deb6e75d6dc1d130e8
SiD <shinde.sidhesh@gmail.com>
2016-05-27 13:53:56 +0530

    added the jQuery file and gave the parallax effect to the text articles

---
+++ js/parallax.js
@@ -0,0 +1,10 @@
+$(function(){
+	$(window).bind('scroll', function(e){
+		parallax();
+	});
+});
+
+function parallax(){
+	var scrollPosition = $(window).scrollTop();
+	$('#stars').css('top',(0-(scrollPosition * .2)) + 'px');
+}
